c048884a4eba8745b9f6794de8a5b70ed79796b9
zoyapetrocenko <petrochenkozoya22@gmail.com>
2024-11-14 20:37:13 +0300

    Initial commit

---
+++ app/src/routes/AppRoutes.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import { LandingPage } from '../pages/LandingPage/LandingPage'
+import { RootLayout } from '../pages/RootLayout/RootLayout'
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<RootLayout/>}> {/* тут лежат странички с хедером и футером*/}
+                <Route index element={<LandingPage/>}/>
+                {/* <Route path="design" element={<DesignPage/>}/> */}
+            </Route>
+
+        </Routes>
+    )
+}
